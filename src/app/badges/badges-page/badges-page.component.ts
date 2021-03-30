1a35f2f2e237a9c16dd19f1739c2d6fdbdaaedcb
Nazarii Karlyk <karliknazar@gmail.com>
2021-03-30 19:41:09 +0300

    complete: challenges block

---
+++ src/app/badges/badges-page/badges-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-badges',
+  templateUrl: './badges-page.component.html',
+  styleUrls: ['./badges-page.component.scss']
+})
+export class BadgesPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/badges/badges-page/badges-page.module.ts
@@ -0,0 +1,22 @@
+import { BadgesPageComponent } from './badges-page.component';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from 'src/app/material/material.module';
+
+const routes = [
+  {
+    path: '',
+    component: BadgesPageComponent
+  }
+];
+
+@NgModule({
+  declarations: [BadgesPageComponent],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class BadgesPageModule { }
